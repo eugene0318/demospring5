2416af8e0549057329e74164e7bf6e574a9641ab
eugene0318 <nttott@gmail.com>
2022-07-27 01:22:59 +0900

    @Autowired와 component scan

--- demospring51/src/main/java/me/whiteship/demospring51/BookService.java
+++ demospring51/src/main/java/me/whiteship/demospring51/BookService.java
@@ -1,5 +1,28 @@
 package me.whiteship.demospring51;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
 
+    @Autowired
+    BookRepository myBookRepository;
+    //List<BookRepository> bookRepositories;
+    
+//    public void printBookRepository() {
+//    	//System.out.println(bookRepository.getClass());
+//    	this.bookRepositories.forEach(System.out::println);
+//    }
+    
+    @PostConstruct
+    public void setup() {
+    	System.out.println(myBookRepository.getClass());
+    }
+
 }
+
--- demospring51/src/main/java/me/whiteship/demospring51/Demospring51Application.java
+++ demospring51/src/main/java/me/whiteship/demospring51/Demospring51Application.java
@@ -1,13 +1,55 @@
 package me.whiteship.demospring51;
 
+import java.util.function.Supplier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
+
+import me.whiteship.out.MyService;
 
 @SpringBootApplication
 public class Demospring51Application {
+	
+	@Autowired 
+	MyService myService;
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demospring51Application.class, args);
+		//SpringApplication.run(Demospring51Application.class, args);
+		var app = new SpringApplication(Demospring51Application.class);
+		
+//		 app.addInitializers(new ApplicationContextInitializer<GenericApplicationContext>() {
+//
+//			@Override
+//			public void initialize(GenericApplicationContext ctx) {
+//				// TODO Auto-generated method stub
+//				ctx.registerBean(MyService.class);
+//			}    // GenericApplicationContext로 설정
+//	           
+//	        });
+		app.addInitializers((ApplicationContextInitializer<GenericApplicationContext>) ctx -> {
+		
+			ctx.registerBean(MyService.class);
+			ctx.registerBean(ApplicationRunner.class, new Supplier<ApplicationRunner>() {	
+					@Override
+					public ApplicationRunner get() {
+						// TODO Auto-generated method stub
+						return new ApplicationRunner() {
+							
+							@Override
+							public void run(ApplicationArguments args) throws Exception {
+								// TODO Auto-generated method stub
+								System.out.println("Funcional Bean Definition!!");
+							}
+						};
+					}
+				});
+			});
+		app.run(args);
 	}
 
 }
